457ec98097c5b52393b61a86b793e61da786d70a
Vinko90 <33526702+Vinko90@users.noreply.github.com>
2023-07-09 10:21:34 +0200

    Add dummy app

---
+++ src/Demo.Versioning.App/Program.cs
@@ -0,0 +1,7 @@
+﻿using DemoVersioning.Core;
+
+Console.WriteLine("Versions:");
+
+Console.WriteLine($"Assembly Version: {Tool.GetAssemblyVersion()}");
+Console.WriteLine($"File Version: {Tool.GetFileVersion()}");
+Console.WriteLine($"Product Version: {Tool.GetProductVersion()}");
---
+++ src/DemoVersioning.Core/Tool.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+
+namespace DemoVersioning.Core
+{
+    public static class Tool
+    {
+        public static string GetAssemblyVersion() => Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
+        public static string GetFileVersion() => FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).FileVersion;
+
+        public static string GetProductVersion() => FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).ProductVersion;
+    }
+}
